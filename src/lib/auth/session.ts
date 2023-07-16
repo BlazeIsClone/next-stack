c30478ee18b91c33bb17792702869dcb04836b01
BlazeIsClone <64116737+BlazeIsClone@users.noreply.github.com>
2023-07-16 18:48:31 +0530

    remove: unwanted session function

--- src/lib/auth/session.ts
+++ src/lib/auth/session.ts
@@ -1,32 +1,6 @@
 import { getServerSession } from "next-auth";
-import { eq } from "drizzle-orm";
 import { authOptions } from "./auth-options";
-import { db } from "~/database";
-import { users } from "~/database/schema";
 
 export const getSession = async () => {
-  const session = await getServerSession(authOptions);
-
-  return session;
-};
-
-export const getCurrentUser = async () => {
-  const session = await getServerSession(authOptions);
-
-  if (!session) {
-    return null;
-  }
-
-  const [currentUser] = await db
-    .select({
-      userId: users.id,
-      email: users.email,
-      name: users.name,
-      image: users.image,
-      createdAt: users.created_at,
-    })
-    .from(users)
-    .where(eq(users.id, session.user.id));
-
-  return currentUser;
+  return await getServerSession(authOptions);
 };
