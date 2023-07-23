f057d2883d760d155db5b7eab01477ded90f38fd
BlazeIsClone <64116737+BlazeIsClone@users.noreply.github.com>
2023-07-23 14:33:28 +0530

    refactor: change database from mysql to planetscale

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,15 +1,14 @@
-import { drizzle } from "drizzle-orm/mysql2";
-import mysql from "mysql2/promise";
+import { drizzle } from "drizzle-orm/planetscale-serverless";
+import { connect } from "@planetscale/database";
 import { env } from "~/env.mjs";
 
 let connection;
 
 try {
-  connection = await mysql.createConnection({
+  connection = connect({
     host: env.MYSQL_HOST,
-    user: env.MYSQL_USER,
+    username: env.MYSQL_USER,
     password: env.MYSQL_PASSWORD,
-    database: env.MYSQL_DATABASE,
   });
 } catch (error) {
   throw new Error(`Failed to establish a database connection.`);
